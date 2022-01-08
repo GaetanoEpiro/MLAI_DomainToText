4840701a3165b6e0e6d72e3e456c39e1cdfcdc31
GaetanoEpiro <s277875@studenti.polito.it>
2022-01-08 11:55:29 +0100

    WIP

--- data/parser.py
+++ data/parser.py
@@ -14,8 +14,19 @@ for line in lines:
     out.write("\t\t" + "\"image_name\": " + "\"" + fields[0] + "\",\n")
     out.write("\t\t" + "\"descriptions:\": [\n")
     for i in range(8):
-        out.write("\t\t\t\"\",\n")
-    out.write("\t\t\t\"\"\n")
+        if i == 0:
+            out.write("\t\t\t\"high-level\",\n")
+        elif i == 3:
+            out.write("\t\t\t\"yes, colorful\",\n")
+        elif i == 5:
+            out.write("\t\t\t\"yes\",\n")
+        elif i == 6:
+            out.write("\t\t\t\"no\",\n")
+        elif i == 7:
+            out.write("\t\t\t\"yes, photo\",\n")
+        else:
+            out.write("\t\t\t\"\",\n")
+    out.write("\t\t\t\"yes, realistic\"\n")
     out.write("\t\t]\n")
     out.write("\t},\n")
 
