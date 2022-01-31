c2d78a660ec5d556b97f71eda18fe83d4e878fa0
GaetanoEpiro <s277875@studenti.polito.it>
2022-01-31 09:10:23 +0100

    Add phrase frequency dictionary

--- utils/split_phrases.py
+++ utils/split_phrases.py
@@ -1,27 +1,49 @@
 import json
 import os
+import collections
 
 path = '/content/MLAI_DomainToText/data_api/data/'
 
+def write_dictionary(dictionary, output):
+  sorted_x = sorted(dictionary.items(), reverse=True, key=lambda kv: kv[1])
+  sorted_dict = collections.OrderedDict(sorted_x)
+
+  complete_path = path + output + '_freq.txt'
+
+  with open(complete_path, 'w') as f:
+    for key in list(sorted_dict.keys()):
+      f.write(str(key) + " : " + str(dictionary[key]) + '\n')
+
+  f.close()
+
+def insert_in_dictionary(dictionary, element):
+  # Check if the word is already in dictionary
+  if element in dictionary:
+    # Increment count of word by 1
+    dictionary[element] = dictionary[element] + 1
+  else:
+    # Add the word to dictionary with count 1
+    dictionary[element] = 1
+
 def split_phrases(): 
-    img_data_dict = dict()
     with open(os.path.join(path, 'image_descriptions.json'), 'r') as f:
-        with open(path + 'images_descr.json', 'w') as f2:
+        with open(path + 'tmp.json', 'w') as f2:
             data = json.load(f)
             for img_d in data:
                 for img in img_d['descriptions']:
-                    texts = img.split(",")
+                    texts = img.split(", ")
                     for txt in texts:
                         " ".join(txt.split())
-                        f2.write(txt)
-                        f2.write("\n")
+                        f2.write(str(txt) + '\n')
+    
+    f.close()
+    f2.close()
                 
-
-
-    with open(os.path.join(path, 'images_descr.json'), 'r') as text:
+    with open(os.path.join(path, 'tmp.json'), 'r') as text:
         
         # Create an empty dictionary
         d = dict()
+        phrases = dict()
             
         # Loop through each line of the file
         for line in text:
@@ -31,28 +53,22 @@ def split_phrases():
             # Convert the characters in line to 
             # lowercase to avoid case mismatch
             line = line.lower()
-            
+
+            insert_in_dictionary(phrases, line)
+
             # Split the line into words
             words = line.split(" ")
             
             # Iterate over each word in line
             for word in words:
-                # Check if the word is already in dictionary
-                if word in d:
-                    # Increment count of word by 1
-                    d[word] = d[word] + 1
-                else:
-                    # Add the word to dictionary with count 1
-                    d[word] = 1
+                insert_in_dictionary(d, word)
             
         # Print the contents of dictionary
-        import collections
-        sorted_x = sorted(d.items(),reverse=True, key=lambda kv: kv[1])
-        sorted_dict = collections.OrderedDict(sorted_x)
-        with open(path + 'phrase_freq.txt', 'w') as f3:
-            for key in list(sorted_dict.keys()):
-                f3.write(key + " : " + str(d[key]))
-                f3.write("\n")
+        write_dictionary(d, 'word')
+        write_dictionary(phrases, 'phrase')
+  
+    text.close()
+    os.remove(os.path.join(path, 'tmp.json'))
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     split_phrases()
